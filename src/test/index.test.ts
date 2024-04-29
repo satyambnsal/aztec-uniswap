089d7045f63b7434e4ef709d1d365b31595e98d7
Po <chenjunfeng@ethstorage.io>
2024-04-29 09:01:20 -0700

    transfer token error

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -28,8 +28,8 @@ describe("UniswapV2", () => {
 
         wallets = await getInitialTestAccountsWallets(pxe);
         accounts = wallets.map(w => w.getCompleteAddress())
-        alice = await createAccount(pxe);
-        bob = await createAccount(pxe);
+        alice = wallets[0];
+        bob = wallets[1];
         token0 = await TokenContract.deploy(alice, alice.getCompleteAddress(), 'Token0', 'TokenSymbol0', 18)
         .send()
         .deployed();
@@ -40,6 +40,11 @@ describe("UniswapV2", () => {
         uniswap = await UniswapV2Contract.deploy(wallets[0], token0.address, token1.address)  
         .send()
         .deployed();
+
+        const recipientAddr = uniswap.address;
+        let alice_addr = alice.getAddress();
+        await token0.methods.mint_public(alice_addr, new Fr(100n)).send().wait();
+        await token0.methods.mint_public(recipientAddr, new Fr(100n)).send().wait();
     }, 120_000)
 
     // it("It increase UniswapV2 funds on mint", async () => {
@@ -55,8 +60,43 @@ describe("UniswapV2", () => {
         await token0.methods.mint_public(recipientAddr, 100n).send().wait();
 
         expect(await token0.methods.balance_of_public(recipientAddr).simulate()).toEqual(100n);
-        let reciept = await uniswap.methods.tranfer_token0(alice_addr,10n, 0n).send().wait();
+        let reciept = await uniswap.methods.tranfer_token0(10n, 0n).send().wait();
         expect(await token0.methods.balance_of_public(recipientAddr).simulate()).toEqual(90n);
         expect(await token0.methods.balance_of_public(alice_addr).simulate()).toEqual(10n);
     }, 15_000)
+
+    it("It add liquidity", async() => {
+        const recipientAddr = uniswap.address;
+        let alice_addr = alice.getAddress();
+
+        console.log("token0 address:", token0.address);
+        console.log("alice_addr address:", alice_addr);
+        console.log("uniswap address===============>:", recipientAddr);
+
+        await token0.methods.mint_public(alice_addr, 100n).send().wait();
+        await token1.methods.mint_public(alice_addr, 100n).send().wait();
+        let resp = await uniswap.withWallet(alice).methods.mint(10, 10, 0, 1).send().wait();
+        console.log(resp);
+
+        // expect(await token0.methods.balance_of_public(alice_addr).simulate()).toEqual(90n);
+        // expect(await token1.methods.balance_of_public(alice_addr).simulate()).toEqual(90n);
+        // expect(await token0.methods.balance_of_public(recipientAddr).simulate()).toEqual(10n);
+        // expect(await token1.methods.balance_of_public(recipientAddr).simulate()).toEqual(10n);
+
+    }, 15_000)
+
+    it.only("It transfer token from user to UNI contract", async () => {
+        const recipientAddr = uniswap.address;
+        let alice_addr = alice.getAddress();
+
+        console.log("token0 address:", token0.address);
+        console.log("alice_addr address:", alice_addr);
+        console.log("uniswap address===============>:", recipientAddr);
+        // expect(await token0.methods.balance_of_public(alice_addr).simulate()).toEqual(100n);
+        // await token0.methods.transfer_public(alice_addr, recipientAddr, 10n, 0).send().wait();
+        // expect(await token0.methods.balance_of_public(alice_addr).simulate()).toEqual(90n);
+
+        let resp = await uniswap.withWallet(alice).methods.tranfer_token0(new Fr(10n), 0).send().wait();
+        console.log(resp);
+    })
 });
