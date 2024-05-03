b36be19106fbe42a5b343aa7f8ff8eb5fa80bf4b
Po <chenjunfeng@ethstorage.io>
2024-05-03 10:35:33 -0700

    test mint,swap,burn publicly succeeds

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -46,7 +46,8 @@ describe('UniswapV2', () => {
     bob = wallets[1]
 
     try {
-      await publicDeployAccounts(alice, [alice])
+      await publicDeployAccounts(alice, [alice, bob])
+
     } catch (err) {
       console.warn('Account is already deplyed')
     }
@@ -71,7 +72,7 @@ describe('UniswapV2', () => {
     secret_hash = computeMessageSecretHash(secret)
   }, 120_000)
 
-  it.only('It add liquidity publicly', async () => {
+  it('It add liquidity publicly succeeds', async () => {
     let alice_addr = alice.getAddress()
 
     await token0.methods.mint_public(alice_addr, 100n).send().wait()
@@ -84,27 +85,28 @@ describe('UniswapV2', () => {
     const nonce_zero = new Fr(0n)
 
     /** Public authwit start */
-
     const action0 = token0
       .withWallet(alice)
       .methods.transfer_public(alice_addr, uniswap.address, 10n, nonce_zero)
-
     const action1 = token1
       .withWallet(alice)
       .methods.transfer_public(alice_addr, uniswap.address, 10n, nonce_zero)
-
     await alice.setPublicAuthWit({ caller: uniswap.address, action: action0 }, true).send().wait()
-
     await alice.setPublicAuthWit({ caller: uniswap.address, action: action1 }, true).send().wait()
-
     /** Public Authwith end*/
-    await uniswap.withWallet(alice).methods.mint(10n, 10n, 0, 0).send().wait()
+
+    let [amount0, amount1] = [10n, 10n]
+    await uniswap.withWallet(alice).methods.mint(amount0, amount1, 0, 0).send().wait()
 
     expect(await token0.methods.balance_of_public(alice_addr).simulate()).toEqual(90n)
     expect(await token1.methods.balance_of_public(alice_addr).simulate()).toEqual(90n)
 
-    expect(await uniswap.methods.get_reserves_0().simulate()).toEqual([10n, 0n, 0n, 0n])
-    expect(await uniswap.methods.get_reserves_1().simulate()).toEqual([10n, 0n, 0n, 0n])
+    expect(await uniswap.methods.get_reserves_0().simulate()).toEqual([amount0, 0n, 0n, 0n])
+    expect(await uniswap.methods.get_reserves_1().simulate()).toEqual([amount1, 0n, 0n, 0n])
+    expect(await token0.methods.balance_of_public(uniswap.address).simulate()).toEqual(amount0)
+    expect(await token1.methods.balance_of_public(uniswap.address).simulate()).toEqual(amount1)
+    expect(await token0.methods.balance_of_public(alice_addr).simulate()).toEqual(100n - amount0)
+    expect(await token1.methods.balance_of_public(alice_addr).simulate()).toEqual(100n - amount1)
   }, 120_000)
 
   it('It swaps publicly from token0 to token1 throws due to lack of liquity', async () => {
@@ -118,7 +120,7 @@ describe('UniswapV2', () => {
     await expect(
       uniswap
         .withWallet(alice)
-        .methods.swap(amount0_in, amount1_out, amount1_in, amount0_out, 0)
+        .methods.swap(amount0_in, amount1_out, amount1_in, amount0_out, 1,1 )
         .send()
         .wait()
     ).rejects.toThrow('Liquidity is not enough!')
@@ -126,22 +128,77 @@ describe('UniswapV2', () => {
 
   it('It swaps publicly from token0 to token1 succeeds', async () => {
     let alice_addr = alice.getAddress()
-    await token0.methods.mint_public(alice_addr, 100n).send().wait()
+    await token0.methods.mint_public(alice_addr, 200n).send().wait()
     await token1.methods.mint_public(alice_addr, 100n).send().wait()
 
+    let nonce = 0;
     // add liquity by Alice first
-    await uniswap.withWallet(alice).methods.mint(100n, 100n, 0, 0).send().wait()
-    expect(await uniswap.methods.get_reserves_0().simulate()).toEqual([100n, 0n, 0n, 0n])
-    expect(await uniswap.methods.get_reserves_1().simulate()).toEqual([100n, 0n, 0n, 0n])
-    // swap by Bob
+    /** Public authwit start */
+    const action0 = token0
+      .withWallet(alice)
+      .methods.transfer_public(alice_addr, uniswap.address, 100n, nonce)
+    const action1 = token1
+      .withWallet(alice)
+      .methods.transfer_public(alice_addr, uniswap.address, 100n, nonce)
+    await alice.setPublicAuthWit({ caller: uniswap.address, action: action0 }, true).send().wait()
+    await alice.setPublicAuthWit({ caller: uniswap.address, action: action1 }, true).send().wait()
+    /** Public Authwith end*/
+    await uniswap.withWallet(alice).methods.mint(100n, 100n, 0, nonce).send().wait()
+
+    /** Public authwit start */
+    // nonce += 1
     let [amount0_in, amount1_out, amount1_in, amount0_out] = [2n, 1n, 0n, 0n]
+    const action2 = token0
+      .withWallet(alice)
+      .methods.transfer_public(alice_addr, uniswap.address, amount0_in, nonce)
+    await alice.setPublicAuthWit({ caller: uniswap.address, action: action2 }, true).send().wait()
+    /** Public Authwith end*/
+
+    // swap by alice
     await uniswap
-      .withWallet(bob)
-      .methods.swap(amount0_in, amount1_out, amount1_in, amount0_out, 0)
+      .withWallet(alice)
+      .methods.swap(amount0_in, amount1_out, amount1_in, amount0_out, nonce, nonce)
       .send()
       .wait()
     expect(await uniswap.methods.get_reserves_0().simulate()).toEqual([102n, 0n, 0n, 0n])
     expect(await uniswap.methods.get_reserves_1().simulate()).toEqual([99n, 0n, 0n, 0n])
+    expect(await token0.methods.balance_of_public(uniswap.address).simulate()).toEqual(102n)
+    expect(await token1.methods.balance_of_public(uniswap.address).simulate()).toEqual(99n)
+    expect(await token0.methods.balance_of_public(alice_addr).simulate()).toEqual(98n)
+    expect(await token1.methods.balance_of_public(alice_addr).simulate()).toEqual(1n)
+  }, 120_000)
+
+  it.only('It removes liqidity publicly succeeds', async () => {
+    let alice_addr = alice.getAddress()
+    await token0.methods.mint_public(alice_addr, 100n).send().wait()
+    await token1.methods.mint_public(alice_addr, 100n).send().wait()
+
+    let nonce = 0;
+    // add liquity by Alice first
+    /** Public authwit start */
+    const action0 = token0
+      .withWallet(alice)
+      .methods.transfer_public(alice_addr, uniswap.address, 100n, nonce)
+    const action1 = token1
+      .withWallet(alice)
+      .methods.transfer_public(alice_addr, uniswap.address, 100n, nonce)
+    await alice.setPublicAuthWit({ caller: uniswap.address, action: action0 }, true).send().wait()
+    await alice.setPublicAuthWit({ caller: uniswap.address, action: action1 }, true).send().wait()
+    /** Public Authwith end*/
+    await uniswap.withWallet(alice).methods.mint(100n, 100n, 0, nonce).send().wait()
+
+    // remove liquidity
+    await uniswap
+      .withWallet(alice)
+      .methods.burn()
+      .send()
+      .wait()
+    expect(await uniswap.methods.get_reserves_0().simulate()).toEqual([0n, 0n, 0n, 0n])
+    expect(await uniswap.methods.get_reserves_1().simulate()).toEqual([0n, 0n, 0n, 0n])
+    expect(await token0.methods.balance_of_public(uniswap.address).simulate()).toEqual(0n)
+    expect(await token1.methods.balance_of_public(uniswap.address).simulate()).toEqual(0n)
+    expect(await token0.methods.balance_of_public(alice_addr).simulate()).toEqual(100n)
+    expect(await token1.methods.balance_of_public(alice_addr).simulate()).toEqual(100n)
   }, 120_000)
 
   it('It swaps privately from token0 to token1 succeeds', async () => {
@@ -162,6 +219,8 @@ describe('UniswapV2', () => {
       .wait()
     expect(await uniswap.methods.get_reserves_0().simulate()).toEqual([102n, 0n, 0n, 0n])
     expect(await uniswap.methods.get_reserves_1().simulate()).toEqual([99n, 0n, 0n, 0n])
+    expect(await token0.methods.balance_of_public(uniswap.address).simulate()).toEqual(99n)
+    expect(await token1.methods.balance_of_public(uniswap.address).simulate()).toEqual(102n)
     // redeem sheild successfully
     // await token1.methods.redeem_shield(bob.getAddress(), 1n, secret).send().wait();
   }, 30_000)
